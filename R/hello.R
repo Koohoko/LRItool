610de76666465c3c1238a04d534b59a08c45e942
Koohoko <Koohoko@gmail.com>
2019-02-25 15:26:57 +0800

    initial

---
+++ R/hello.R
@@ -0,0 +1,17 @@
+library(devtools)
+
+.onAttach <- function(libname, pkgname){
+    packageStartupMessage("LRIflu\nFor disccussion, please contact: hggu@connect.hku.hk")
+}
+
+#' Add together two numbers
+#'
+#' @param x A number
+#' @param y A number
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+add <- function(x, y) {
+    x + y
+}
